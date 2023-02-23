01d8fbf256fb8585b0533812d48b602b3a379228
Eduardo Viegas <engenedu@hotmail.com>
2023-02-23 17:01:11 -0300

    Criação de Strings, definição de cores, temas e dimensões

--- app/src/main/java/com/innaval/omeletefake/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/innaval/omeletefake/presentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.innaval.omeletefake
+package com.innaval.omeletefake.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.innaval.omeletefake.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
